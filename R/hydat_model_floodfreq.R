41bfb14420819ba4d349c3c49fe67151108caa07
David Tavernini <david.tavernini@uleth.ca>
2016-12-03 20:56:59 -0800

    transitioned diag plots to ggplot

--- R/hydat_model_floodfreq.R
+++ R/hydat_model_floodfreq.R
@@ -5,7 +5,7 @@
 #' @param x input dataset post-processed through `hydat_load`
 #' @param logx logical input on whether to transform x variable with log10()
 #' @param logy logical input on whether to transform y variable with log10()
-#'
+#' @import ggplot2
 #' @return returns linear model diagnostics and model result in a list format
 #' @export
 
@@ -21,31 +21,45 @@ hydat_model_floodfreq <- function(x, logx = FALSE, logy = FALSE){
   flooddist$yhat <- fitted(flm)
   flooddist$resid <- residuals(flm)
   flooddist$stdresid <- residuals(flm)/sumflm$sigma
-
+  # Apply theme to all plots
+  theme_dtavern <- theme(panel.background = element_rect(fill = "white"),
+                         axis.line.y = element_line(colour="black", size = 0.25),
+                         axis.line.x = element_line(colour="black", size = 0.25),
+                         axis.title.x = element_text(face = "bold", size = 13),
+                         axis.title.y = element_text(face = "bold", size = 13, margin = margin(10,10,5,10)),
+                         axis.text.x = element_text(face = "bold", size = 10),
+                         axis.text.y = element_text(size = 9),
+                         legend.key = element_rect(fill = "white"),
+                         legend.title = element_text(face = "bold", size = 11),
+                         legend.text = element_text(size = 11),
+                         legend.position = c(0.9, 0.6))
   ## diagnostic plots
-
-    par(mfrow=c(2,2),cex=1.0,mai=c(1.1,1.1,0.5,0.4))
   # Fitted line plot. Predicted y versus observed y.
-  plot(flooddist$yhat~flooddist$rec,xlab="recurrence",
-       ylab="yhat",main="Predicted vs Actual",
-       pch=19)
-  abline(a=0,b=1,col="red")
+  pvhat_plot <- ggplot(data = flooddist)+
+    geom_point(aes(x=rec,y=yhat)) +
+    labs(x="Recurrence",y="yhat", title = "Predicted vs. Actual") +
+    geom_abline(intercept = 0, slope = 1) +
+    theme_dtavern
   # Residual plot
-  plot(flooddist$yhat, flooddist$resid,xlab = "yhat",
-       ylab = "Residual",main = "Residual Plot",pch = 19)
-  abline(h=0, col="red")
+  resid_plot <- ggplot(data = flooddist)+
+    geom_point(aes(x=yhat, y=resid))+
+    labs(x="yhat", y="Residual", title = "Residual plot")+
+    geom_hline(yintercept = 0) +
+    theme_dtavern
   # Normality plot
-  qqnorm(flooddist$resid, ylab = "Residual", pch =19)
-  qqline(flooddist$resid, col ="red")
+  qq_plot <- ggplot(data=flooddist) +
+    stat_qq(aes(sample = resid)) +
+    labs(x="Expected", y="Observed", title = "QQ Plot")+ ## Add QQ line?
+    theme_dtavern
   # Histogram of residuals
-  hist(flooddist$resid,breaks = 10,xlab = "Residual",
-       main = "Residuals", col="grey", border="black")
-  par(mfrow=c(1,1),cex=1.0,mai=c(1.0,1.0,1.0,1.0))
-  diag_plots <-recordPlot()
-  plot.new()
+  resid_hist <- ggplot(data=flooddist)+
+    geom_histogram(aes(x = resid), bins = 10)+
+    labs(x="Residual", y="Frequency", title = "Residual histogram")+
+    theme_dtavern
 
+  plots <- list(pvhat_plot, resid_plot, qq_plot, resid_hist)
   shapiro <- shapiro.test(flooddist$resid)
-  output <- list(diagnostic_plots = diag_plots, shapiro_normality = shapiro, model = flm, model_summary = sumflm)
+  output <- list(diagnostic_plots = plots, shapiro_normality = shapiro, model = flm, model_summary = sumflm)
   return(output)
 
 }
